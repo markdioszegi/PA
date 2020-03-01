6861bb52a528ac1c1fd7c8be767af85dc2225359
Márk Diószegi <walnut5588@gmail.com>
2020-03-01 18:17:40 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Practice3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
